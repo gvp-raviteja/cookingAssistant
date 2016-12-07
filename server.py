c0bd8919c0016557afe7ab7002c8837fd6639b95
gvp-raviteja <ganireddy.raviteja@gmail.com>
2016-12-07 18:29:47 -0500

    trying get favicon.ico

--- server.py
+++ server.py
@@ -3,20 +3,21 @@ __author__ = 'hemalatha_ganireddy'
 import os
 from flask import Flask
 import sys
-sys.path.append('../logic/')
 
 import main
 import json
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def sam():
+    return True
 
 @app.route('/')
 def index():
     # output = main.ma()
     # json.loads(output)['result']['metadata']['intentName']
     return "Hello, World!"
-
 
 port = int(os.environ.get("PORT", 5000))
 app.run(debug=True, port=port)
